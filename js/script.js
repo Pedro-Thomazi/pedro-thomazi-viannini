b59277d72e2cdafb1e5cf8c2b0242088933392a6
Pedro Thomazi Viannini <pedro.thomazi@hotmail.com>
2025-04-10 15:08:46 -0300

    Ajustes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const containerDarkMode = document.querySelector(".btnDarkMode")
 const labelDM = document.querySelector(".labelDM")
+const colorYellow = document.querySelector(".colorYellow")
+const colorGray = document.querySelector(".colorGray")
 
 labelDM.addEventListener("click", () => {
   labelDM.classList.toggle("active")
+  containerDarkMode.classList.toggle("gray")
+  colorYellow.classList.toggle("hide")
+  colorGray.classList.toggle("hide")
 })
