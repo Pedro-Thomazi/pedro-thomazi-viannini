a510e9d388ad6f6897a9f7136eac14b4e18dee40
Pedro Thomazi Viannini <pedro.thomazi@hotmail.com>
2025-04-10 12:44:46 -0300

    Melhorando Portfólio

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const containerDarkMode = document.querySelector(".btnDarkMode")
+const labelDM = document.querySelector(".labelDM")
+
+labelDM.addEventListener("click", () => {
+  labelDM.classList.toggle("active")
+})
